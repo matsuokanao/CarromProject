fe969e838ee25a74162360f1caaa9be901138f4e
松岡 奈央 <matsuokanao1029@gmail.com>
2021-03-12 14:56:27 +0900

    クリア時のText表示

---
+++ Assets/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject clearUi;
+    GameObject[] objects;
+   
+    void Start()
+    {
+        clearUi.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        objects = GameObject.FindGameObjectsWithTag("white");
+
+        if(objects.Length == 0){
+
+           clearUi.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/GreenGameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenGameController : MonoBehaviour
+{
+
+    public GameObject clearUi;
+    GameObject[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clearUi.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objects = GameObject.FindGameObjectsWithTag("brown");
+
+        if(objects.Length == 0){
+
+           clearUi.SetActive(true);
+
+        }
+    }
+}
