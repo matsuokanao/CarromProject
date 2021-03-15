e407f0bd3e19c1d46d719917fdb6c0eaab0d643c
松岡 奈央 <matsuokanao1029@gmail.com>
2021-03-15 11:44:18 +0900

    Queen実装

---
+++ Assets/RedCoinController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedCoinController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+      //tagがgoalの場合、gameObjを削除する
+       
+
+        if (collider.gameObject.tag == "goal" && GreenGameManager.GreenplayerTurn == true)
+        {
+            //Score クラスの score 変数に +1 点する
+            GreenScore.Greenscore += 3;
+            //削除
+            Destroy(gameObject);
+            
+        }
+        if (collider.gameObject.tag == "goal" && GameManager.playerTurn == true)
+        {
+            //Score クラスの score 変数に +1 点する
+            Score.score += 3;
+            //削除
+            Destroy(gameObject);
+            
+        }
+    }
+}
+
